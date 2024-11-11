59559456638977d50cc36910cb5d8c27d5ae6b32
linscot10 <linscot10@gmail.com>
2024-11-11 13:39:52 +0300

    assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=input('Whats your name Mate? : ')
+age=input('Whats your age Mate? : ')
+location=input('Whats your location Mate? : ')
+
+print(f"My Name is {name} I'm {age} years old and come from {location} city" )
