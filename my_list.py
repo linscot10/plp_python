5b52d505c51dec3ed649db1b015d028eee16ee5e
linscot10 <linscot10@gmail.com>
2024-11-11 14:07:49 +0300

    week 2

---
+++ my_list.py
@@ -0,0 +1,19 @@
+my_list=[ 10, 20, 30, 40]
+
+
+
+my_list[1]=15
+my_other=[50, 60, 70]
+my_list.extend(my_other)
+
+my_list.remove(70)
+
+my_list.sort()
+
+# position=len(my_list)
+
+Index=my_list.index(30)
+print(Index)
+
+print(my_list)
+
